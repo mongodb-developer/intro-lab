55584dce41f0df7f95a9763b41d70cf289d9ece8
Joel Lord <joelphy@gmail.com>
2023-09-28 08:46:18 -0400

    Removed the announcement bar

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -130,14 +130,7 @@ const config = {
           hideable: true,
         },
       },
-      announcementBar: {
-        id: "feedback_form",
-        content:
-          'We are looking to revamp our docs, please fill <a target="_blank" rel="noopener noreferrer" href="#">this survey</a>',
-        backgroundColor: "#fafbfc",
-        textColor: "#091E42",
-        isCloseable: true,
-      },
+      announcementBar: undefined,
       navbar: {
         title: `${title}`,
         logo: {
